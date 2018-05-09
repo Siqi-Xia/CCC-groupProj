77481704532f4aa47668171f7ff2781cd1b88491
sheng15 <tspower@qq.com>
2018-05-09 20:48:43 +1000

    check twitter duplication

---
+++ checkDuplication.py
@@ -0,0 +1,55 @@
+##author Sheng Tang
+##09/05/2018  20:22
+
+
+######################################################################################################
+#############	for shell to check whether a given file is already in the couchdb,     ###############
+############# 	performance can be improved with a better couchdb design。             ###############
+###################################################################################################### 
+
+
+
+
+
+
+import pycouchdb
+import json
+import sys
+
+def checkDuplicaton(server,db,file):
+
+
+	#pass http://username:password@ip_address:5984/ to server constructor:
+	server = pycouchdb.Server(server)
+	db = server.database(db)
+
+	f=open(file,"r")
+	if f.mode =='r' :
+		contents = f.read()
+		data=json.loads(contents)
+		#data = json.loads(json.loads(n))
+
+	twitid=str(data['id'])
+	#print(type(twitid))
+
+	not_duplicated = True
+	idmap = list(db.query("id/id_str"))
+	for item in idmap:
+		#print(type(item["key"]))
+		if twitid == item["key"]:
+			not_duplicated = False
+
+	return not_duplicated
+
+
+## return 0 if not duplicated, 1 if duplicated
+## eg:  if[ $? -eq 0 ]
+
+if __name__=='__main__':
+	test=checkDuplicaton(sys.argv[1],sys.argv[2],sys.argv[3])	
+	if test:
+		#print("not_duplicated")
+		sys.exit(0)
+	else :
+		#print("duplicated")
+		sys.exit(1)
