beff806fcf75344702adeb4c1927eae6c61fc70c
Kathy Xia <xsq1903@gmail.com>
2018-05-10 00:45:07 +1000

    minor update
    minor update

--- server_set.sh
+++ server_set.sh
@@ -5,26 +5,31 @@ echo "Set up instance"
 python Boto/run_boto.py
 var=$(cat result.txt)
 
-sed -i '' "s/host/$var/g" myplatform/hosts
+echo $var
+
+xargs sed -i '' "s/host/$var/g" myplatform/hosts
 
 
 echo "Run basic settings"
-ansible-playbook -K myplatform/playbook.yml --tags "basic"
+ansible-playbook -s myplatform/playbook.yml --tags "basic"
 
 echo "Mount volumes"
-ansible-playbook -K myplatform/playbook.yml --tags "mount"
+ansible-playbook -s myplatform/playbook.yml --tags "mount"
 
 echo "Set up couchdb"
-ansible-playbook -K ./myplatform/playbook.yml --tags "couchdb"
+ansible-playbook -s myplatform/playbook.yml --tags "couchdb"
 
 echo "Set up couchdb clustering"
-ansible-playbook -K myplatform/playbook.yml --tags "cluster"
+ansible-playbook -s myplatform/playbook.yml --tags "cluster"
 
 echo "Install harvest app"
-ansible-playbook -K myplatform/playbook.yml --tags "harvest"
+ansible-playbook -s myplatform/playbook.yml --tags "harvest"
 
 echo "Install analysis app"
-ansible-playbook -K myplatform/playbook.yml --tags "analysis"
+ansible-playbook -s myplatform/playbook.yml --tags "analysis"
 
 echo "Install webserver"
-ansible-playbook -K myplatform/playbook.yml --tags "webserver"
+ansible-playbook -s myplatform/playbook.yml --tags "webserver"
+
+
+
